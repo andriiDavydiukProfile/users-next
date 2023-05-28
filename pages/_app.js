797700d2ef4b2d8427f7dc71c53e454bad2b14bb
a.davydiuk <adavydiuk@liveperson.com>
2023-05-29 01:06:49 +0300

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import React from 'react';
+import { Provider } from 'react-redux';
+import '../../../next_migration/my-app/pages/reset.css';
+import Head from "next/head";
+import { wrapper, store } from "@/store/store";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({ Component, pageProps }) {
+  return (
+      <>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        </Head>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </>
+  );
 }
+
+
+export default wrapper.withRedux(MyApp);
